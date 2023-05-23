1798630df87bbffba1a56fb8da14e66fd2724562
RandomNick7 <107753721+RandomNick7@users.noreply.github.com>
2023-05-24 00:32:47 +0300

    Updated Lexer

--- Lexer/lexer.cpp
+++ Lexer/lexer.cpp
@@ -1,252 +1,65 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <string>
 #include <cctype>
-#include <vector>
-
-//Simple Example:  =>20-10><10{int kek <~ 10} WORKS
-//TODO: Check for ignorable strings: escape char \		quote " and '	Comment, single and multi 
 
 using namespace std;
-int line=1, col=1;
 
-void show_error(string s, string* code){
-	cout << "Error at line " << line << ", column " << col << ": " << s << endl;
-	cout << "Here -> " << *code << endl;
-}
-
-int determine_num(string* code, vector<pair<string,string>>* lexemes){
-	int i=0;
-	while(isdigit((*code)[i])){
-		i++;
-	}
-	col+=i;
+void remove_space(string* code){
+	int i=0, len = code->size();
 	
-	if(i==0){
-		if((*code)[i]=='-'){
-			show_error("No negative numbers as indices allowed!",code);
-		}else{
-			show_error("Missing number",code);
-		}
-		return 1;
-	}
-	
-	if((*code)[i]=='.'){
+	while(isspace((*code)[i]) && i<len){
 		i++;
-		while(isdigit((*code)[i])){
-			i++;
-		}
-		col+=i;
-		
-		if((*code)[i-1]=='.'){
-			show_error("No digits after decimal point found",code);
-			return 1;
-		}
-		lexemes->push_back({code->substr(0,i),"CONST_FLOAT"});
-	}else{
-		lexemes->push_back({code->substr(0,i),"CONST_INT"});
 	}
 	code->erase(0,i);
+}
+
+void check_type(string* code, vector<pair<string,string>>* lexemes){
+	int i=0;
 	
-	//If there is a negative sign in front and there is no number previous to that
-	//Then this number is simply negative, no need for negative sign token
-	if(lexemes->size()>2){
-		if(	lexemes->end()[-2].second.compare("OP_SUB") == 0 &&
-			lexemes->end()[-3].second.compare("CONST_INT") != 0 &&
-			lexemes->end()[-3].second.compare("CONST_FLOAT") != 0){
-				lexemes->erase(lexemes->end()-2);
-				lexemes->back().first.insert(0,"-");
-		}
-	}else if(lexemes->size()==2){
-		if(lexemes->end()[-2].second.compare("OP_SUB") == 0){
-			lexemes->erase(lexemes->end()-2);
-			lexemes->back().first.insert(0,"-");
-		}
-	}
-	return 0;
-}
-
-int determine_id(string* code, vector<pair<string,string>>* lexemes){
-	if(isalpha((*code)[0])){
-		int i = 1;
-		while(isalnum((*code)[i]) || (*code)[i] == '_'){
-			i++;
-		}
-		lexemes->push_back({code->substr(0,i),"IDENTIFIER"});
-		//Eliminate whitespaces until next token
-		while(isspace((*code)[i])){
-			i++;
-		}
-		code->erase(0,i);
-		return 0;
-	}else{
-		show_error("Variables can't start with a non-letter",code);
-		return 1;
-	}
-}
-
-int determine_type(string* code, vector<pair<string,string>>* lexemes){
-	string type[]{
+	vector<string> type{
 		"int","flt","chr","str","bool"
 	};
-	string token[]{
+	vector<string> token{
 		"TYPE_INT","TYPE_FLOAT","TYPE_CHAR","TYPE_STRING","TYPE_BOOL"
 	};
-	
-	int i=0, t, types_len = end(type)-begin(type);
 	
 	while(isalpha((*code)[i])){
 		i++;
 	}
-	col+=i;
+	string buffer = code->substr(0,i);
+	code->erase(0,i);
 	
-	bool is_type = false;
-	string temp = code->substr(0,i);
-	for(t=0; t < types_len && !is_type; t++){
-		is_type = (temp.compare(type[t])==0);
-	}
-	
-	if(is_type){
-		t--;
-		code->erase(0,i);
-		lexemes->push_back({type[t],token[t]});
-		
-		//If identifier has '#' after it, it is an array instead. Repeat.
-		int dimensions = 1;
-		while((*code)[0]=='#'){
-			code->erase(0,1);
-			col++;
-			
-			int err = determine_num(code,lexemes);
-			if(err == 0){
-				if(lexemes->back().second.compare("CONST_INT") == 0){
-					//Append "ARR" to the value of the second to last lexeme for each dimension
-					lexemes->end()[-1-dimensions].second.insert(0,"ARR_");
-				}else{
-					show_error("Floats cannot determine array sizes",code);
-					return 1;
-				}
-			}else{
-				return err;
-			}
-			dimensions++;
+	for(int i=0;i<type.size();i++){
+		if(buffer == type[i]){
+			lexemes->push_back({buffer,token[i]});
+			return;
 		}
-		return 0;
-	}else{
-		//Not a type, check if valid word instead
-		return determine_id(code,lexemes);
 	}
+	lexemes->push_back({buffer,"IDENTIFIER"});
 }
 
-int determine_symbol(string* code, vector<pair<string,string>>* lexemes){
-	char ops[]{
-		'+',	//Add
-		'-',	//Subtract
-		'*',	//Multiply
-		'/',	//Divide
-		'%',	//Modulo Division
-		'&',	//Binary AND
-		'^',	//Binary OR
-		'!',	//Binary NOT
-		'>',	//Greater than
-		'<',	//Less than
-		'\\',	//Escape
-		',',	//Comma Separator
-		'(',	//Open Parenthesis
-		')',	//Closed Parenthesis
-		'[',	//Open Braket
-		']',	//Closed Braket
-		'{',	//Open Brace
-		'}'		//Closed Brace
-	};
+void check_num(string* code, vector<pair<string,string>>* lexemes){
+	int i=0;
+	string type = "CONST_INT";
 	
-	string compound_ops[]{
-		"<~",	//Assign
-		"><",	//Equal to
-		"<>",	//Different to
-		">=",	//Greater or Equal
-		"<="	//Less or Equal
-	};
-	
-	string token[]{
-		"OP_ADD",
-		"OP_SUB",
-		"OP_MULT",
-		"OP_DIV",
-		"OP_MOD",
-		"OP_AND",
-		"OP_OR",
-		"OP_NOT",
-		"OP_GT",
-		"OP_LT",
-		"SYM_ESC",
-		"SYM_SEP",
-		"SYM_PAREN_L",
-		"SYM_PAREN_R",
-		"SYM_BRKET_L",
-		"SYM_BRKET_R",
-		"SYM_BRACE_L",
-		"SYM_BRACE_R",
-		"OP_ASSGN",
-		"OP_EQ",
-		"OP_NEQ",
-		"OP_GTE",
-		"OP_LTE"
-	};
-	
-	int i=0, o, op_count = end(ops)-begin(ops), 
-		c_op_count = end(compound_ops)-begin(compound_ops);
-	
-	char special[]{'-',',','#','\"','(',')','[',']','{','}'};
-	bool is_special = false;
-	//Do not count above symbols behind other chars
-
-	while(ispunct((*code)[i])){
-		if(i>0){
-			for(int j=0;j<end(special)-begin(special) && !is_special;j++){
-				is_special = (*code)[i]==special[j];
-			}
-			if(is_special){
-				break;
-			}
-		}
+	while(isdigit((*code)[i])){
 		i++;
 	}
-	
-	string temp = code->substr(0,i);
-	
-	bool is_op = false;
-	if(temp.size()==1){
-		for(o=0; o<op_count && !is_op; o++){
-			is_op = (temp[0]==ops[o]);
+	if((*code)[i] == '.'){
+		i++;
+		while(isdigit((*code)[i])){
+			i++;
 		}
-		if(is_op){
-			o--;
-			code->erase(0,i);
-			lexemes->push_back({temp,token[o]});
-		}else{
-			show_error("Unrecognized symbol!",code);
-			return 1;
-		}
-	}else{
-		for(o=0; o<c_op_count && !is_op; o++){
-			is_op = (temp.compare(compound_ops[o]) == 0);
-		}
-		if(is_op){
-			o--;
-			code->erase(0,i);
-			lexemes->push_back({temp,token[o+op_count]});
-		}else{
-			show_error("Unrecognized symbols!",code);
-			return 1;
-		}
+		type = "CONST_FLOAT";
 	}
-	return 0;
+	lexemes->push_back({code->substr(0,i),type});
+	code->erase(0,i);
 }
 
-int determine_keyword(string* code, vector<pair<string,string>>* lexemes){
-	string keyword[]{
-		"||>",		//Import
+void check_keyword(string* code, vector<pair<string,string>>* lexemes, int* exception){
+	vector<string> keyword{
 		"<~",		//Assign
 		"=>",		//If
 		"#=",		//Else
@@ -255,11 +68,40 @@ int determine_keyword(string* code, vector<pair<string,string>>* lexemes){
 		">>=>",		//Function Declaration
 		"|^|",		//Return Statement
 		">]",		//Scan
-		"[<"		//Print
+		"[<",		//Print
+		"<~",		//Assign
+		"><",		//Equal to
+		"<>",		//Different to
+		">=",		//Greater or Equal
+		"<=",		//Less or Equal
+		"%>",		//Block Comment
+		"/>"		//Line Comment
 	};
 	
-	string token[]{
-		"IMPORT",
+	vector <char> ops{
+		'(',	//Open Parenthesis
+		')',	//Closed Parenthesis
+		'[',	//Open Braket
+		']',	//Closed Braket
+		'{',	//Open Brace
+		'}',	//Closed Brace
+		',',	//Comma Separator
+		'+',	//Add
+		'-',	//Subtract
+		'*',	//Multiply
+		'/',	//Divide
+		'%',	//Modulo Division
+		'&',	//Binary AND
+		'|',	//Binary OR
+		'^',	//Binary XOR
+		'!',	//Binary NOT
+		'>',	//Greater than
+		'<',	//Less than
+		'"',	//Quotation
+		'#'		//Array Size
+	};
+	
+	vector<string> token{
 		"ASSIGN",
 		"IF_COND",
 		"ELSE_COND",
@@ -268,119 +110,117 @@ int determine_keyword(string* code, vector<pair<string,string>>* lexemes){
 		"FUNCTION_DEF",
 		"RETURN",
 		"READ",
-		"WRITE"
+		"WRITE",
+		"OP_ASSGN",
+		"COMP_EQ",
+		"COMP_NEQ",
+		"COMP_GTE",
+		"COMP_LTE",
+		"COMMENT_BLOCK_L",
+		"COMMENT_LINE",
+		"SYM_PAREN_L",
+		"SYM_PAREN_R",
+		"SYM_BRKET_L",
+		"SYM_BRKET_R",
+		"SYM_BRACE_L",
+		"SYM_BRACE_R",
+		"SYM_SEP",
+		"OP_ADD",
+		"OP_SUB",
+		"OP_MULT",
+		"OP_DIV",
+		"OP_MOD",
+		"OP_AND",
+		"OP_OR",
+		"OP_XOR",
+		"OP_NOT",
+		"COMP_GT",
+		"COMP_LT",
+		"QUOTATION",
+		"ARR_SIZE"
 	};
 	
-	int i=0,k,keyword_len = end(keyword)-begin(keyword);
+	int i=0;
 	while(ispunct((*code)[i])){
 		i++;
 	}
+	string buffer = code->substr(0,i);
 	
-	//Do not consider open brackets after keyword
-	if((*code)[i] == '{' || (*code)[i]=='('){
-		i--;
+	for(i=0;i<keyword.size();i++){
+		if(buffer.rfind(keyword[i],0) != string::npos){
+			code->erase(0,keyword[i].size());
+			if(keyword.size()-i < 3){
+				*exception = keyword.size()-i;
+			}else{
+				lexemes->push_back({keyword[i],token[i]});
+			}
+			return;
+		}
 	}
 	
-	bool is_keyword = false;
-	string temp = code->substr(0,i);
-	for(k=0; k < keyword_len && !is_keyword; k++){
-		is_keyword = (temp.compare(keyword[k])==0);
+	for(int j=0;j<ops.size();i++,j++){
+		if((*code)[0] == ops[j]){
+			string temp(1,ops[j]);
+			lexemes->push_back({temp,token[i]});
+			if(ops[j] == '"'){
+				*exception = 3;
+			}
+			break;
+		}
 	}
 	
-	if(is_keyword){
-		k--;
-		code->erase(0,i);
-		lexemes->push_back({keyword[k],token[k]});
-		return 0;
-	}else{
-		//Not a keyword, check if valid symbol instead
-		return determine_symbol(code,lexemes);
-	}
+	code->erase(0,1);
 }
 
-int lexer(string* code, vector<pair<string,string>>* lexemes, string* buffer, int* exception){
-	int err = 0;
-	while(code->size()>0 && err==0){
-		if(*exception == 0){
-			int i=0;
-			while(isspace((*code)[i])){
-				i++;
-			}
-			code->erase(0,i);
-			
-			if(code->size()>1){
-				if((*code)[0]=='/' && (*code)[1]=='>'){
-					lexemes->push_back({"/>","COMMENT_LINE"});
-					code->erase(0,2);
-					lexemes->push_back({*code,"COMMENT_BODY"});
-					break;
-				}
-				
-				if((*code)[0]=='%' && (*code)[1]=='>'){
-					lexemes->push_back({"%>","COMMENT_BLOCK_L"});
-					code->erase(0,2);
-					*exception = 2;
-					break;
-				}
-			}
-			
-			//If there is a " and it is not escaped...
-			if((*code)[0]=='\"' && lexemes->end()[-1].second.compare("SYM_ESC") != 0){
-				lexemes->push_back({"\"","QUOTATION"});
-				code->erase(0,1);
-				*exception = 1;
-			}
-			if(*exception!=1){
-				if(isalpha((*code)[0])){
-					err = determine_type(code, lexemes);
-				}else if(isdigit((*code)[0])){
-					err = determine_num(code, lexemes);
-				}else if(ispunct((*code)[0])){
-					err = determine_keyword(code, lexemes);
-				}
-			}
-		}
-		
-		if(*exception == 1){
-			int i=0;
-			while((*code)[i]!='\"' && i<code->size()){
-				i++;
-			}
-			
-			if(i==code->size()){
-				show_error("No matching quotation mark found on line!",code);
-				return 1;
-			}
-			
-			i++;
-			code->erase(0,i);
-			lexemes->push_back({code->substr(0,i),"CONST_STRING"});
-			lexemes->push_back({"\"","QUOTATION"});
-			*exception = 0;
-		}
-		
-		if(*exception == 2){
-			int i=1;
-			while(((*code)[i-1]!='<' || (*code)[i]!='%') && i<code->size()){
-				*buffer += *code;
-			}
-			if(i!=code->size()){
-				lexemes->push_back({*buffer,"COMMENT_BODY"});
-				lexemes->push_back({"<%","COMMENT_BLOCK_R"});
-				buffer->clear();
-				*exception = 0;
-			}
-			return 0;
+void lexer(string code, vector<pair<string,string>>* lexemes, int* exception){
+	int i=0;
+	
+	while(code.size()>0 && *exception == 0){
+		//Depending on what character code stopped at, call one of the follwoing functions:
+		lexer_loop:
+		char c = code[0];
+		if(isalpha(c)){
+			check_type(&code,lexemes);
+		}else if(isdigit(c)){
+			check_num(&code, lexemes);
+		}else if(ispunct(c)){
+			check_keyword(&code, lexemes, exception);
+		}else if(isspace(c)){
+			remove_space(&code);
+		}else{
+			code.erase(0,1);
 		}
 	}
-	return err;
+	
+	if(*exception == 1){
+		*exception = 0;
+	}else if(*exception == 2){
+		size_t search = code.find("<%");
+		if(search != string::npos){
+			code.erase(0,search+2);
+			*exception = 0;
+		}
+	}else{
+		size_t search = code.find('"');
+		if(search!=string::npos){
+			if(search>0){
+				lexemes->push_back({code.substr(0,search),"CONST_STR"});
+			}
+			lexemes->push_back({"\"","QUOTATION"});
+			code.erase(0,search+1);
+			*exception = 0;
+			if(code.size()>0){
+				goto lexer_loop;
+			}
+		}
+	}
 }
 
 int main(int argc, char* argv[]){
 	ifstream source_code;
 	vector<pair<string,string>> lexemes;
-	string src_line, buffer;
-	int exception = 0, err=0;
+	string src_line;
+	int exception = 0;
 	
 	if(argc==1){
 		cout << "Input file path:\n";
@@ -391,22 +231,20 @@ int main(int argc, char* argv[]){
 		source_code.open(argv[1]);
 	}
 	
-	
 	if(source_code.is_open()){
-		while(getline(source_code,src_line) && err==0){
-			err = lexer(&src_line,&lexemes,&buffer,&exception);
-			line++;
-			col=1;
+		while(getline(source_code,src_line)){
+			lexer(src_line,&lexemes, &exception);
 		}
 		
-		if(err == 0){
-			for(int i=0;i<lexemes.size();i++){
-				cout << "{\"" << lexemes[i].first << "\", " << lexemes[i].second << "}" << endl;
-			}
+		ofstream out_file;
+		out_file.open("../Parser/token_list.txt");
+
+		for(int i=0;i<lexemes.size();i++){
+			out_file << lexemes[i].second << " \"" << lexemes[i].first << "\"" << endl;
 		}
+		out_file.close();
 	}else{
-		cout << "File not found!";
-		err = 1;
+		cout << "File not found!" << endl;
 	}
-	return err;
+	return 0;
 }
